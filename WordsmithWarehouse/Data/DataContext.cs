dea10914b2e089c210f4e11c7efe8bf3fd9b460d
Murat <134512780+DuarteMurat@users.noreply.github.com>
2023-08-16 17:49:48 +0100

    InitDb and CRUD Books Slight layout changes

---
+++ WordsmithWarehouse/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+using Microsoft.EntityFrameworkCore;
+
+namespace WordsmithWarehouse.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
