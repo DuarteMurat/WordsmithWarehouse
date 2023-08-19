4ee1c351b225930e8b28d02da66485a0fa702cb9
Bruno Ferreira <bff3rreira03@gmail.com>
2023-08-19 17:41:40 +0100

    Tag Repository fix

---
+++ WordsmithWarehouse/Repositories/Classes/TagRepository.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using WordsmithWarehouse.Data;
+using WordsmithWarehouse.Repositories.Interfaces;
+
+namespace WordsmithWarehouse.Repositories.Classes
+{
+    public class TagRepository : GenericRepository<Tag>, ITagRepository
+    {
+        private readonly DataContext _context;
+        public TagRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetComboTags()
+        {
+            var list = _context.Tags.Select(t => new SelectListItem
+            {
+                Text = t.Name,
+                Value = t.Id.ToString()
+            }).OrderBy(l => l.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "Select a Tag",
+                Value = "0"
+            });
+
+            return list;
+        }
+    }
+}
